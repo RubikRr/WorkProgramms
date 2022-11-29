7b34af9916298ff58274eaba3f08758871c47cfb
Ukropchik <vova.chakalov.2003@gmail.com>
2022-11-29 21:32:31 +0300

    Прописал классы для подключения к бд

--- ExcelToWordProject/Utils/DBMySQLUtils.cs
+++ ExcelToWordProject/Utils/DBMySQLUtils.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace ExcelToWordProject.Utils
 {
     internal class DBMySQLUtils
     {
+        public static MySqlConnection GetDbConnection(string host, int port, string database, string username, string password)
+        {
+            String connString = "Server=" + host + ";Database=" + database
+                                + ";port=" + port + ";User Id=" + username + ";password=" + password;
+            MySqlConnection conn = new MySqlConnection(connString);
+            return conn;
+        }
     }
 }
--- ExcelToWordProject/Utils/DBUtils.cs
+++ ExcelToWordProject/Utils/DBUtils.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace ExcelToWordProject.Utils
 {
     internal class DBUtils
     {
+        public static MySqlConnection GetDBConnection()
+        {
+            //string host = "192.168.205.130";
+            //int port = 3306;
+            //string database = "simplehr";
+            //string username = "root";
+            //string password = "1234";
+
+            //return DBMySQLUtils.GetDbConnection(host, port, database, username, password);
+        }
     }
 }
