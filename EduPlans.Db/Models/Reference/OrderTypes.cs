3a081eebf8cb8e50fcd9cf8db9ca7a253d23e33a
Ukropchik <vova.chakalov.2003@gmail.com>
2023-05-29 21:50:37 +0300

    отрефакторил название свойства

--- EduPlans.Db/Models/Reference/OrderTypes.cs
+++ EduPlans.Db/Models/Reference/OrderTypes.cs
@@ -11,7 +11,7 @@ namespace EduPlans.Db.Models
     public class OrderTypes
     {
         public int Id { get; set; }
-        public string type { get; set; }
+        public string Type { get; set; }
 
     }
 
