f7376eb87b61b8228d7bdc04671a05bc9c438628
Ukropchik <vova.chakalov.2003@gmail.com>
2023-05-29 21:47:33 +0300

    создал справочную таблицу форм занятий

---
+++ EduPlans.Db/Models/Reference/SubjectForms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlans.Db.Models
+{
+    [Table("subject_forms")]
+    public class SubjectForms
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
