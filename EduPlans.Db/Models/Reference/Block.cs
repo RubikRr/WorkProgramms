3eeef84d9fbda5ebe6e5cea1fbd45be7c0e3b5fb
Ukropchik <vova.chakalov.2003@gmail.com>
2023-05-29 21:45:14 +0300

    добавил справочную таблицу блоков обучения

---
+++ EduPlans.Db/Models/Reference/Block.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlans.Db.Models
+{
+    public class Block
+    {
+        public int Id { get; set; }
+        [Column("block_title")]
+        public string BlockTitle { get; set; }
+        [Column("part_title")]
+        public string PartTitle { get; set; }
+
+
+    }
+}
