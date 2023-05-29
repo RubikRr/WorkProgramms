d4d0839c4627d889e9c9c08ce6460811b480e199
Ukropchik <vova.chakalov.2003@gmail.com>
2023-05-29 21:46:09 +0300

    создал модель справочной таблицы форм обучения

---
+++ EduPlans.Db/Models/Reference/EduForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlans.Db.Models
+{
+    [Table("edu_forms")]
+    public class EduForm
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
