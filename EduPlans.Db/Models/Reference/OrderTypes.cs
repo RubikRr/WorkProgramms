f589753885659939b6e1484dedab73696499e328
Ukropchik <vova.chakalov.2003@gmail.com>
2023-05-29 21:47:11 +0300

    создал справочную таблицу типов приказов

---
+++ EduPlans.Db/Models/Reference/OrderTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlans.Db.Models
+{
+    [Table("order_types")]
+    public class OrderTypes
+    {
+        public int Id { get; set; }
+        public string type { get; set; }
+
+    }
+
+}
