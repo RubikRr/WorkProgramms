869c19b78636c362f889c0c897cc933b22c1d609
Ukropchik <vova.chakalov.2003@gmail.com>
2023-06-06 19:20:16 +0300

    добавил связывающие таблицы

---
+++ EduPlans.Db/Models/Binding/EduPlanCompetencies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlans.Db.Models.Binding
+{
+    [Table("edu_plan_competencies")]
+    public class EduPlanCompetencies
+    {
+        public int Id { get; set; }
+        [Column("competency_id")]
+        public int CompetencyId { get; set; }
+
+        [Column("edu_plan_id")]
+        public int EduPlanId { get; set; }
+    }
+}
---
+++ EduPlans.Db/Models/Binding/EduPlanFormControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlans.Db.Models.Binding
+{
+    [Table("edu_plan_form_control")]
+    public class EduPlanFormControl
+    {
+        public int Id { get; set; }
+        [Column("edu_semester_id")]
+        public int EduSemesterId { get; set; }
+        [Column("form_control_id")]
+        public int FormControlId { get; set; }
+    }
+}
