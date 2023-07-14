a614bd09d7f7bb913c4bf84cbe91f31250de59fe
luf-fy <margiev.erik@list.ru>
2023-07-15 01:40:41 +0300

    Create FormControlContext

---
+++ EduPlans.Db/Сontexts/Reference/FormControlContext.cs
@@ -0,0 +1,27 @@
+﻿using EduPlans.Db.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlans.Db.Сontexts.Reference
+{
+    internal class FormControlContext : DbContext
+    {
+        public DbSet<FormControl> FormControls { get; set; }
+
+        public FormControlContext() : base("EduPlansDb") { }
+
+        public FormControl GetFormControl(string title)
+        {
+            return FormControls.FirstOrDefault(c => c.Title == title);
+        }
+
+        public int GetFormControlId(string title)
+        {
+            return GetFormControl(title)?.Id ?? 0;
+        }
+    }
+}
